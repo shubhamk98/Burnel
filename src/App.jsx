64f1baade0377b592d663e3b2837a82026729479
Shubham kamboj <kambojshubham277@gmail.com>
2024-05-07 11:55:30 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Logo from "./assets/logo.svg"
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <img src={Logo}></img>
+    </div>
+  );
+};
+
+export default App;
